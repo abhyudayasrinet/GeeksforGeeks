89e09838405448b590c9755ef02503caabd5eca6
Abhyudaya Srinet <abhyu195@gmail.com>
2018-09-20 20:15:59 -0700

    solved more tree questions

--- interview questions/lis.cpp
+++ interview questions/lis.cpp
@@ -2,14 +2,22 @@
 
 #include <cstdio>
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int a[1000];
 int s[1000];
 int main()
 {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+    
     int n;
     cin>>n;
+    memset(s, 0, sizeof(int)*1000);
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
@@ -20,12 +28,15 @@ int main()
     {
         for(int j=0;j<i;j++)
         {
-            if(a[i] > a[j])
+            if(a[i] > a[j] && s[i] < s[j] + 1)
                 s[i] = s[j] + 1;
         }
         if(s[i] > max)
             max = s[i];
     }
+    if(n==1)
+        max = 0;
     cout<<max<<endl;
+    }
     return 0;    
 }
---
+++ interview questions/maxtreepathsum.cpp
@@ -0,0 +1,116 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+	int data;
+	Node *left, *right;
+};
+Node *newNode(int data)
+{
+    Node *node = new Node;
+    node->data = data;
+    node->left = node->right = NULL;
+    return (node);
+}
+int maxPathSum(struct Node *root);
+void insert(Node *root, int a1,int a2, char lr){
+	if(root==NULL)
+		return ;
+	if(root->data==a1){
+		switch(lr){
+			case 'L':root->left=newNode(a2);
+					break;
+			case 'R':root->right=newNode(a2);
+					break;
+		}
+	}
+	insert(root->left,a1,a2,lr);
+	insert(root->right,a1,a2,lr);
+}
+void inorder(Node *root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right)
+;}
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		Node *root=NULL;
+		while(n--){
+			int a1,a2;
+			char lr;
+			cin>>a1>>a2>>lr;
+			if(root==NULL){
+				root=newNode(a1);
+				switch(lr){
+					case 'L':root->left=newNode(a2);
+							break;
+					case 'R':root->right=newNode(a2);
+							break;
+				}
+			}
+			else
+				insert(root,a1,a2,lr);
+		}
+		//inorder(root);
+		//cout<<endl;
+		cout<<maxPathSum(root)<<endl;		
+	}
+}
+
+
+/*Please note that it's Function problem i.e.
+you need to write your solution in the form of Function(s) only.
+Driver Code to call/invoke your function is mentioned above.*/
+
+/*Complete the function below
+Node is as follows
+struct Node{
+	int data;
+	Node *left, *right;
+};
+*/
+
+int max(int a, int b)
+{
+    if(a > b)
+        return a;
+    return b;
+}
+
+int helper(Node *root, int &maxval)
+{
+    if(root->left == NULL && root->right == NULL)
+        return root->data;
+    
+    int left_val=NULL, right_val=NULL;
+    if(root->left != NULL)
+        left_val = helper(root->left);
+    if(root->right != NULL)
+        right_val = helper(root->right);
+    
+    if(left_val != NULL && right_val != NULL)
+    {
+        if(maxval < left_val + right_val + root->data)
+        {
+            maxval = left_val + right_val + root->data;
+        }
+    }
+    if(left_val == NULL)
+        return root->data + right_val;
+    if(right_val == NULL)
+        return root->data + left_val;
+    return max(root->data + left_val, root->data + right_val);
+}
+
+int maxPathSum(struct Node *root)
+{
+    int maxval = INT_MIN;
+    helper(root, &maxval);
+    return maxval;
+}
